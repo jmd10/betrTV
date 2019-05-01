f67338a82851c0fda44d26418b726e2bee2783a0
jmd_10 <jason@lvl6.xyz>
2019-05-01 07:44:24 -0700

    Created index. validated working on local host and online

--- betrTV_app/views.py
+++ betrTV_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    ''' The home page for betrTV_app '''
+
+    return render(request, 'betrTV_app/index.html')
+    
