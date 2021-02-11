1a5d5cb2e2343f0869abf3ffca25c16aa43cd5bb
jmd_10 <jmd_10@yahoo.com>
2021-02-10 19:33:40 -0500

    add meta / SEO tags

--- betrTV_app/views.py
+++ betrTV_app/views.py
@@ -23,10 +23,16 @@ def index(request):
     analytics = "<!-- Global site tag (gtag.js) - Google Analytics --> \
         <script async src='https://www.googletagmanager.com/gtag/js?id=G-5S9053QREN'></script> \
         <script>window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);} gtag('js', new Date());gtag('config', 'G-5S9053QREN');</script>"
+    meta = "<meta name='description' content='The aim of betr.tv is to take videos on the web (youtube, vimeo, et... ) and collect them in one place.\
+        These videos are videos such as Impact Theory, Mindvalley, Jay Shetty, Tim Ferris, etc... videos that empower \
+        and inform. It is intended to be stripped of all of the banal shit that is out there. All of the popular \
+        platforms are dragged to the lowest common denominator. This is intended to be a place people can go to not\
+        have to endure the barrage of shit they get eslewhere. Instead they will be exposed to empowering content./>'\
+        <meta name='robots' content='index, follow' />"
 
-    html = '<html><head><title>BetrTV</title>%s<head/><body><link href="https://fonts.googleapis.com/css?family=Gugi"\
+    html = '<html><head><title>Empowerment on Demand</title>%s%s<head/><body><link href="https://fonts.googleapis.com/css?family=Gugi"\
         + rel="stylesheet"><h1 style="color: grey; font-family: Gugi; font-size: \
         + 20px;">BetrTV... Empowerment on Demand</h1>\
-        <iframe %s></iframe></body></html>' %(analytics, video)
+        <iframe %s></iframe></body></html>' %(analytics, meta, video)
 
     return HttpResponse(html)
